9b6589fb29159ab78eba8873fab3d28f071f9d81
Noguchi Hiroshi <a1316hn@aiit.ac.jp>
2013-09-18 03:45:54 +0000

    first commit

---
+++ enp_test/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EnpTest::Application.config.secret_key_base = 'd96ab1cb8e35d97aa66c13e93f8e3db0513aef33eabf4a6f12c9b244389c6a076deb990fab79ad561cd041f4fb96993a941262fcedfb046698d92750946ff796'
